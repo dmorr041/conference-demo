910c3f20686bdbebaacd80dd5bcd0dd775e76f4b
Darien Morrison <darien.morrison@jbhunt.com>
2020-07-13 09:20:01 -0500

    Added the Sessions Controller

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,33 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController             // This will respond to payloads incoming/outgoing as JSON REST endpoints
+@RequestMapping("/api/v1/sessions")     // Tells the router what the mapping URL will look like. All requests at this url will be sent here
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+}
